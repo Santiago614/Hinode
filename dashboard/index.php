2dc8fa1c0224718c5d7937ee18302d532c9f89b2
Santiago614 <sgomez9002@misena.edu.co>
2022-04-06 09:00:55 -0500

    Cambios en la extensión de archivo

--- dashboard/index.php
+++ dashboard/index.php
@@ -50,7 +50,7 @@ if (isset($_SESSION['correo'])) {
       <!-- Sidebar -->
       <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
         <!-- Sidebar - Brand -->
-        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
           <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-laugh-wink"></i>
           </div>
@@ -62,7 +62,7 @@ if (isset($_SESSION['correo'])) {
 
         <!-- Nav Item - Dashboard -->
         <li class="nav-item active">
-          <a class="nav-link" href="index.html">
+          <a class="nav-link" href="index.php">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span></a>
         </li>
